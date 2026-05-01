a4fad41a86a424b7d14537a6f7e104d619444c7a
stackmedia-lab <rizwan.stackmedia@gmail.com>
2026-05-01 17:50:45 +0500

    Update sanity.ts

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@sanity/client';
 
 export const client = createClient({
-  projectId: import.meta.env.VITE_SANITY_PROJECT_ID || 'your-project-id',
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID || 'k8u9z0dx',
   dataset: import.meta.env.VITE_SANITY_DATASET || 'production',
   useCdn: true,
   apiVersion: '2023-05-03',
